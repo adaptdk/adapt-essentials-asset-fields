3fc07dae96271c88b2aca4cc75d1ac33afcbe201
Darius Zivertas <d.zivertas@gmail.com>
2023-12-13 11:30:58 +0200

    Change port to 3000

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,9 @@ function renderChunks(deps: Record<string, string>) {
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
   test: {
     environment: 'happy-dom',
   },
